693b985d0644cb6683ab5f512378202ce7258700
Dead Whale <vjames_kingv@mail.ru>
2020-12-10 19:54:18 +0300

    1

--- 10/Form1.Designer.cs
+++ 10/Form1.Designer.cs
@@ -33,37 +33,42 @@ namespace _10
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.panel1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(467, 279);
+            this.button1.Location = new System.Drawing.Point(8, 80);
             this.button1.Margin = new System.Windows.Forms.Padding(5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(236, 74);
+            this.button1.Size = new System.Drawing.Size(249, 46);
             this.button1.TabIndex = 0;
-            this.button1.Text = "1";
+            this.button1.Text = "Очистить массив";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(467, 111);
+            this.button2.Location = new System.Drawing.Point(8, 28);
             this.button2.Margin = new System.Windows.Forms.Padding(5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(236, 74);
+            this.button2.Size = new System.Drawing.Size(249, 46);
             this.button2.TabIndex = 1;
             this.button2.Text = "Добавить число в массив";
             this.button2.UseVisualStyleBackColor = true;
@@ -71,39 +76,29 @@ namespace _10
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(3, 184);
+            this.textBox1.Location = new System.Drawing.Point(3, 165);
             this.textBox1.Margin = new System.Windows.Forms.Padding(5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(418, 29);
+            this.textBox1.Size = new System.Drawing.Size(265, 29);
             this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 21;
-            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Location = new System.Drawing.Point(3, 0);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(418, 130);
+            this.listBox1.Size = new System.Drawing.Size(265, 130);
             this.listBox1.TabIndex = 3;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(467, 195);
-            this.button3.Margin = new System.Windows.Forms.Padding(5);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(236, 74);
-            this.button3.TabIndex = 0;
-            this.button3.Text = "Вывести элементы в listBox";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(467, 29);
+            this.button4.Location = new System.Drawing.Point(8, 28);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(236, 74);
+            this.button4.Size = new System.Drawing.Size(249, 47);
             this.button4.TabIndex = 4;
-            this.button4.Text = "Расчитать сумму";
+            this.button4.Text = "Расчитать";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
@@ -114,14 +109,23 @@ namespace _10
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Location = new System.Drawing.Point(0, 29);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(424, 240);
+            this.panel1.Size = new System.Drawing.Size(271, 214);
             this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите число";
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(122, 460);
+            this.button6.Location = new System.Drawing.Point(8, 80);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(117, 74);
+            this.button6.Size = new System.Drawing.Size(249, 46);
             this.button6.TabIndex = 4;
             this.button6.Text = "Выйти";
             this.button6.UseVisualStyleBackColor = true;
@@ -129,9 +133,9 @@ namespace _10
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(3, 460);
+            this.button5.Location = new System.Drawing.Point(8, 28);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(113, 74);
+            this.button5.Size = new System.Drawing.Size(249, 46);
             this.button5.TabIndex = 4;
             this.button5.Text = "О программе";
             this.button5.UseVisualStyleBackColor = true;
@@ -143,7 +147,7 @@ namespace _10
             this.менюToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(820, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(816, 24);
             this.menuStrip1.TabIndex = 6;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -170,28 +174,48 @@ namespace _10
             this.выйтиToolStripMenuItem.Text = "Выйти";
             this.выйтиToolStripMenuItem.Click += new System.EventHandler(this.выйтиToolStripMenuItem_Click);
             // 
-            // label1
+            // groupBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(142, 158);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 21);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Введите число";
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(277, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(265, 133);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Кнопки управления таблицей";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Location = new System.Drawing.Point(548, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(265, 133);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Кнопки управления формой";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Location = new System.Drawing.Point(277, 168);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(265, 77);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Задание";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(820, 548);
+            this.ClientSize = new System.Drawing.Size(816, 246);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.menuStrip1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button3);
             this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(5);
@@ -201,6 +225,9 @@ namespace _10
             this.panel1.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -212,7 +239,6 @@ namespace _10
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.MenuStrip menuStrip1;
@@ -222,6 +248,9 @@ namespace _10
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- 10/Form1.cs
+++ 10/Form1.cs
@@ -12,67 +12,73 @@ namespace _10
 {
     public partial class Form1 : Form
     {
+        //Создание новой коллекции для использования в программе
         List<int> mass = new List<int>();
         public Form1()
         {
             InitializeComponent();
         }
-
+        //Очищение массива
         private void button1_Click(object sender, EventArgs e)
         {
-            int res = listBox1.Items.Count;
-            MessageBox.Show(res.ToString());
+            listBox1.Items.Clear();
         }
         private void button2_Click(object sender, EventArgs e)
         {
+            //Добавление элементов в массив: Через коллекцию mass
             mass.Add(Convert.ToInt32(textBox1.Text));
-            textBox1.Clear();
+            //Напрямую в listBox1
             listBox1.Items.Add(Convert.ToInt32(textBox1.Text));
-            listBox1.Items.Clear();
-            listBox1.DataSource = mass;
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
+            //Очищение textBox для удобства 
             textBox1.Clear();
-            
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            int result = 0;
+            //Вывод количества элементов массива по свойсву mass.Count
+            MessageBox.Show("Количество элементов = " + " " + mass.Count + " ");
+            int result = 0;//переменная для вывода с начальным значением 0
+            //Цикл до количества всех элементов listBox
             for (int i = 0; i < listBox1.Items.Count; i++)
             {
+                //Условие кратности 7ми элементов listBox 
                 if ((int)listBox1.Items[i] % 7 == 0)
                 {
+                    //Присваивание раннее заданной переменной result значения элемента listBox, прошедшего проверку на кратность
                     result += Convert.ToInt32(listBox1.Items[i]);
                 }
             }
-            MessageBox.Show(result.ToString());
+            //Вывод result через message box
+            MessageBox.Show("Сумма элементов таблицы кратных 7 = " + result);
         }
-        //О программе(меню)
+        //О программе(Меню)
         private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Работу выполнил студент группы ИСП-31  \n" +
                 "Практическая 10, вариант 2: \n" +
                 "Составьте программу вычисления в массиве суммы всех чисел, кратных 7");
         }
-        //О программе
+        //О программе(Форма)
         private void button5_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Работу выполнил студент группы ИСП-31  \n" +
                 "Практическая 10, вариант 2: \n" +
                 "Составьте программу вычисления в массиве суммы всех чисел, кратных 7");
         }
-
+        //Выйти(Меню)
         private void выйтиToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-
+        //Выйти(Форма)
         private void button6_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        //При нажатии клавиши Enter в textBox произойдет нажатие кнопки "Добавить элемент в массив"
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) button2_Click(sender, e);
         }
     }
 }
