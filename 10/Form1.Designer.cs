0bece15a0fef5da5810ddb222fc6ad57feebc807
ИСП-31 <-31@LAB21-08>
2020-12-10 10:44:31 +0300

    1

--- 10/Form1.Designer.cs
+++ 10/Form1.Designer.cs
@@ -35,22 +35,32 @@ namespace _10
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(27, 292);
+            this.button1.Location = new System.Drawing.Point(467, 279);
             this.button1.Margin = new System.Windows.Forms.Padding(5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(236, 74);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Количество элементов";
+            this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(27, 208);
+            this.button2.Location = new System.Drawing.Point(467, 111);
             this.button2.Margin = new System.Windows.Forms.Padding(5);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(236, 74);
@@ -61,35 +71,35 @@ namespace _10
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(27, 166);
+            this.textBox1.Location = new System.Drawing.Point(3, 184);
             this.textBox1.Margin = new System.Windows.Forms.Padding(5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(236, 29);
+            this.textBox1.Size = new System.Drawing.Size(418, 29);
             this.textBox1.TabIndex = 2;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 21;
-            this.listBox1.Location = new System.Drawing.Point(27, 12);
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(236, 130);
+            this.listBox1.Size = new System.Drawing.Size(418, 130);
             this.listBox1.TabIndex = 3;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(27, 376);
+            this.button3.Location = new System.Drawing.Point(467, 195);
             this.button3.Margin = new System.Windows.Forms.Padding(5);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(236, 74);
             this.button3.TabIndex = 0;
-            this.button3.Text = "Очистить массив";
+            this.button3.Text = "Вывести элементы в listBox";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(271, 208);
+            this.button4.Location = new System.Drawing.Point(467, 29);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(236, 74);
             this.button4.TabIndex = 4;
@@ -97,21 +107,100 @@ namespace _10
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(424, 240);
+            this.panel1.TabIndex = 5;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(122, 460);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(117, 74);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Выйти";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 460);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(113, 74);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "О программе";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(820, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.выйтиToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.сохранитьToolStripMenuItem.Text = "О программе";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.выйтиToolStripMenuItem.Text = "Выйти";
+            this.выйтиToolStripMenuItem.Click += new System.EventHandler(this.выйтиToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите число";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(725, 459);
+            this.ClientSize = new System.Drawing.Size(820, 548);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
             this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,6 +214,14 @@ namespace _10
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выйтиToolStripMenuItem;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- 10/Form1.cs
+++ 10/Form1.cs
@@ -20,22 +20,59 @@ namespace _10
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            int res = listBox1.Items.Count;
+            MessageBox.Show(res.ToString());
         }
         private void button2_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Add(Convert.ToInt32(textBox1.Text));
+            mass.Add(Convert.ToInt32(textBox1.Text));
             textBox1.Clear();
+            listBox1.Items.Add(Convert.ToInt32(textBox1.Text));
+            listBox1.Items.Clear();
+            listBox1.DataSource = mass;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Clear();
+            textBox1.Clear();
+            
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            int result = 0;
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if ((int)listBox1.Items[i] % 7 == 0)
+                {
+                    result += Convert.ToInt32(listBox1.Items[i]);
+                }
+            }
+            MessageBox.Show(result.ToString());
+        }
+        //О программе(меню)
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Работу выполнил студент группы ИСП-31  \n" +
+                "Практическая 10, вариант 2: \n" +
+                "Составьте программу вычисления в массиве суммы всех чисел, кратных 7");
+        }
+        //О программе
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Работу выполнил студент группы ИСП-31  \n" +
+                "Практическая 10, вариант 2: \n" +
+                "Составьте программу вычисления в массиве суммы всех чисел, кратных 7");
+        }
 
+        private void выйтиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
